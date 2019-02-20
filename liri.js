e9ede989ffcf248485415cbef2703a41595c547c
cfern052 <cfern052@gmail.com>
2019-02-19 20:24:04 -0500

    updated spotify and concert

--- liri.js
+++ liri.js
@@ -1,19 +1,29 @@
 require("dotenv").config();
 var moment = require('moment');
+var spotKey=require('./keys.js');
 moment().format();
-var Spotify = require('node-spotify-api');
+var spotify = require('node-spotify-api');
 var axios = require("axios");
 
 var call=process.argv[2];
-var input=parseFloat(process.argv[3]);
+var input=process.argv[3];
 
 if(call == "concert-this"){
 axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-console.log("\nArtist: " + Response.name + "\nVenue name: " + Response.venue[1] + "\nVenue location: " + Response.venue[3] + "\nDate: " + response.datetime)
+.then(function (response){
+// console.log(response);
+console.log("\nArtist: " + input + "\nVenue name: " + response.data.venuedata[1] + "\nVenue location: " + response.data.venuedata[3] + "\nDate: " + response.data.datetime)
+})
 }
 
 if(call == "spotify-this-song"){
-
+    spotify.search({ type: 'track', query: input }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
 }
 
 if(call == "movie-this"){
