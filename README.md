1b04e2de41de2e9ed4e27e52ae847c82ffe555c0
cfern052 <cfern052@gmail.com>
2019-02-22 10:58:52 -0500

    updated readMe and code

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ This Liri Node app is a command line app which uses multiple API's and databases
 
 Use the query "concert-this" followed by a band to find their next concert.
 
+![Example:](./images/concert.png);
+
+Use the query "spotify-this-song" followed by a song name to find out info of the song.
+
+![Example:](./images/song.png);
+
+Use the query "movie-this" followed by a movie title to find more about the film.
+
+![Example:](./images/movie.png);
+
+Use the query "do-what-it-says" to run a call that pulls information from random.txt
+
+![Example:](./images/do.png);
--- liri.js
+++ liri.js
@@ -1,78 +1,77 @@
+
+
 require("dotenv").config();
 var moment = require('moment');
-var spotKey=require('./keys.js');
-// moment().format();
+var spotKey = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
-var fs= require("fs");
+var fs = require("fs");
 var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
- });
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
 
-var call=process.argv[2];
-var input=process.argv[3];
+var call = process.argv[2];
+var input = process.argv[3];
 
 
-function concert(){
+function concert() {
   axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-  .then(function (response){
-    for (var i = 2; i < process.argv.length; i++)
-      var artist=process.argv[i];
-  console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
-  console.log("\nArtist: " + artist + "\nVenue name: " + response.data[0].venue.name + "\nVenue location: " + response.data[0].venue.city + "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-  console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
-  })
-  };
+    .then(function (response) {
+      for (var i = 2; i < process.argv.length; i++)
+        var artist = process.argv[i];
+      console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
+      console.log("\nArtist: " + artist + "\nVenue name: " + response.data[0].venue.name + "\nVenue location: " + response.data[0].venue.city + "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+      console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
+    })
+};
 
-function spot(){
+function spot() {
   {
     for (var i = 2; i < process.argv.length; i++)
-    var track=process.argv[i];
-    spotify.search({ type: 'track', query: track }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       var song=data.tracks.items[0]
+      var track = process.argv[i];
+    spotify.search({ type: 'track', query: track }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      var song = data.tracks.items[0]
       console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
-      console.log("Song: " + song.name + "\nPreview URL: " +song.preview_url +"\nArtist: "+ song.artists[0].name + "\nAlbum: " + song.album.name) 
+      console.log("Song: " + song.name + "\nPreview URL: " + song.preview_url + "\nArtist: " + song.artists[0].name + "\nAlbum: " + song.album.name)
       console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
     });
-}
-}
-
-function movie(){
- {
-    axios.get("http://www.omdbapi.com/?t="+ input +"&y=&plot=short&tomatoes=true&apikey=trilogy").then(
-        function(response) {
-          console.log(response);  
-          console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");  
-          console.log("Movie title: " + response.data.Title + "\nRelease date: " + response.data.Released + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " + response.data.tomatoRating + "\nProduced in : " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors in the film: " + response.data.Actors);
-          console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~");         
-        })
-        };
-    
+  }
 }
 
-function doIt(){
-  fs.readFile("random.txt", "utf8", function(err, data){
-    
+function movie() {
+  {
+    axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
+      function (response) {
+        console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log("Movie title: " + response.data.Title + "\nRelease date: " + response.data.Released + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " + response.data.tomatoRating + "\nProduced in : " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors in the film: " + response.data.Actors);
+        console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~");
+      })
+  };
+
+}
+
+function doIt() {
+  fs.readFile("random.txt", "utf8", function (err, data, ) {
+
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    console.log(data);
-    var dataArr=data.split(",");
-    var call= dataArr[0];
-    var input=dataArr[1];
-  run(call);
+    var dataArr = data.split(",");
+    var call = dataArr[0];
+    var text = dataArr[1];
+    run(call, text)
   });
-  }
+}
 
-function run(call){
-  if(call == "concert-this"){concert()};
-  if(call == "spotify-this-song"){spot()};
-  if(call == "movie-this"){movie()};
-  if(call == "do-what-it-says"){doIt()};
+function run(call) {
+  if (call == "concert-this") { concert() };
+  if (call == "spotify-this-song") { spot() };
+  if (call == "movie-this") { movie() };
+  if (call == "do-what-it-says") { doIt() };
 
 }
 run(call);
